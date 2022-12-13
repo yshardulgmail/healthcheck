f1d2efe75187a119003c8715d05fda4759c0175b
Yogesh Shardul <yogesh@Yogeshs-MacBook-Pro.local>
2022-12-13 19:36:12 +0530

    mauus timer

--- src/healthcheck.js
+++ src/healthcheck.js
@@ -10,13 +10,49 @@ const App = () => {
   const [tabIndex, setTabIndex] = useState(0);
 
   useEffect(() => {
+    // clearTimer(getDeadTime());
     const interval = setInterval(() => {
       console.log("Interval: ", interval);
+      setTimer('00:00:10');
+      // if (Ref.current) clearInterval(Ref.current);
+      let deadline = new Date();
+      startTimer(deadline.setSeconds(deadline.getSeconds() +60));
       window.location.reload();
     }, 60*1000);
+    // Ref.current = id;
   
     return () => clearInterval(interval);
   }, []);
+
+  
+    // The state for our timer
+    const [timer, setTimer] = useState('1:00');
+  
+    // const Ref = useRef(null);
+    const getTimeRemaining = (e) => {
+        const total = Date.parse(e) - Date.parse(new Date());
+        const seconds = Math.floor((total / 1000) % 60);
+        const minutes = Math.floor((total / 1000 / 60) % 60);
+        return {
+            total, minutes, seconds
+        };
+    }
+  
+  
+    const startTimer = (e) => {
+        let { total, minutes, seconds } 
+                    = getTimeRemaining(e);
+        if (total >= 0) {
+  
+            // update the timer
+            // check if less than 10 then we need to 
+            // add '0' at the beginning of the variable
+            setTimer(
+                (minutes > 9 ? minutes : '0' + minutes) + ':'
+                + (seconds > 9 ? seconds : '0' + seconds)
+            )
+        }
+    }
 
   return (
     <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
@@ -27,10 +63,15 @@ const App = () => {
       <TabPanel>
         <div>
           <div>
-            <h1 style={{color: "lightblue"}}>BDX Healthcheck Dashboard</h1>
+            <h1 style={{color: "#009EDC"}}>BDX Healthcheck Dashboard</h1>
           </div>
-          <div>
-            <label>Page with refresh in 1 min</label>
+          <div style={{overflow: "hidden", width: "100%"}}>
+          <button onClick={() => window.location.reload()} className="button modal_send" style={{marginLeft: "10px"}}>
+              Refresh Now!!
+            </button>
+            <label style={{float: "right"}}>Page with refresh in 1 min</label>
+            
+
           </div>
           <div>
             <Example/>
